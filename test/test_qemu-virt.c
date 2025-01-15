f3da36ac8260bf1b331ce708e4d2546695fc9615
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-15 10:29:29 +0100

    test: add test for the dtb_foreach_rsvmap_entry
    The device tree included in the virt machine for riscv the reservation
    map is empty. This test checks that the iterator does not execute the
    body of the iterator.

--- test/test_qemu-virt.c
+++ test/test_qemu-virt.c
@@ -11,4 +11,15 @@ int main(int argc, char **argv)
     } else {
         printf("qemu-virt: dtb_fromptr " ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET);
     }
+
+    bool has_rsvmap_entries = false;
+    dtb_foreach_rsvmap_entry(devicetree, {
+        has_rsvmap_entries = true;
+    });
+    if (has_rsvmap_entries == true) {
+        printf("qemu-virt: dtb_foreach_rsvmap_entry " ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+        exit(1);
+    } else {
+        printf("qemu-virt: dtb_foreach_rsvmap_entry " ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET);
+    }
 }
