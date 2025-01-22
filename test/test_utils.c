fcad4aa8a4555821c801f12da2599f6a021f518a
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-22 15:08:11 +0100

    test: add test skip_until_same_depth

--- test/test_utils.c
+++ test/test_utils.c
@@ -23,4 +23,35 @@ int main(int argc, char **argv)
     } else {
         printf("utils: strcmp_nodename 'memory' != 'soc' " ANSI_COLOR_RED "FAILED" ANSI_COLOR_RESET "\n");
     }
+
+    uint32_t tokenlist[] = {
+        DTB_BEGIN_NODE, 0,  // BEGIN ROOT NODE
+            DTB_NOP,
+            DTB_NOP,
+            DTB_BEGIN_NODE, 0, // cpus
+                DTB_BEGIN_NODE, 0, // cpu0
+                    DTB_NOP,
+                    DTB_NOP,
+                DTB_END_NODE,
+                DTB_BEGIN_NODE, 0, // cpu1
+                    DTB_NOP,
+                    DTB_NOP,
+                DTB_END_NODE,
+            DTB_END_NODE,
+            DTB_BEGIN_NODE, 0, // memory
+                DTB_NOP,
+                DTB_NOP,
+            DTB_END_NODE,
+        DTB_END
+    };
+
+    uint32_t *cpus_node = &tokenlist[4];
+    uint32_t *memory_node = &tokenlist[17];
+    uint32_t *result = skip_until_same_depth(cpus_node);
+    if (result == memory_node) {
+        printf("utils: skip_until_same_depth /cpus -> /memory " ANSI_COLOR_GREEN "SUCCESS" ANSI_COLOR_RESET "\n");
+    } else {
+        printf("utils: skip_until_same_depth /cpus -> /memory " ANSI_COLOR_RED "FAILED" ANSI_COLOR_RESET "\n");
+        exit(1);
+    }
 }
