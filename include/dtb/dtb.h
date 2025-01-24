616dabe4c54dff7cdad4c7a5a8574a5a32f5ee12
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-24 12:55:15 +0100

    parser: add dtb_node_name function

--- include/dtb/dtb.h
+++ include/dtb/dtb.h
@@ -51,6 +51,8 @@ dtb *dtb_fromptr(void *ptr);
 
 dtb_node dtb_find(dtb *devicetree, const char *path);
 
+#define dtb_node_name(node) (char *)((uint32_t *)node+1)
+
 #define dtb_property_name(node) (char *)((uint32_t *)node+1)
 
 #define dtb_foreach_property(dtb, node, x) { \
