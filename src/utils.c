8852af7cdadbf7eeff17fd342f36b2b59471b3d5
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-28 08:54:54 +0100

    parser: add next_token utility

--- src/utils.c
+++ src/utils.c
@@ -24,3 +24,43 @@ int strcmp_nodename(const char *pathpart, const char *nodename)
 
     return 1;
 }
+
+uint32_t *next_token(uint32_t *token)
+{
+    assert((uint64_t)token % 4 == 0);
+
+    if (*token == DTB_BEGIN_NODE) {
+        token++;
+
+        char *tokenchar = (char *)token;
+        while (*tokenchar != '\0') {
+            tokenchar++;
+        }
+
+        tokenchar += (uint64_t)tokenchar % 4;
+        token = (uint32_t *)tokenchar;
+
+        assert(*token != FDT_END);
+    } else if (*token == DTB_END_NODE) {
+        token++;
+
+        assert(*token != DTB_PROP);
+    } else if (*token == DTB_PROP) {
+        uint32_t len = DTB_BYTESWAP32(*(token + 1));
+        if (len % sizeof(uint32_t) == 0) {
+            token += len / sizeof(uint32_t) + 3;
+        } else {
+            uint32_t len_rounding = 4 - (len % 4);
+            token += (len + len_rounding) / sizeof(uint32_t) + 3;
+        }
+
+        assert(*token != DTB_END);
+    } else if (*token == DTB_NOP) {
+        token++;
+    } else {
+        assert(false);
+    }
+
+    assert((uint64_t)token % 4 == 0);
+    return token;
+}
