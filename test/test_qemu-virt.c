9e9ddc01d83e1d5f43378707ef07deced3de0d59
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-29 10:20:16 +0100

    test: add child iterator test

--- test/test_qemu-virt.c
+++ test/test_qemu-virt.c
@@ -34,6 +34,44 @@ int main(int argc, char **argv)
     bool ok = found_compatible == true && found_model == true;
     print_test_result("qemu-virt: dtb_foreach_property '/'", ok);
 
+    bool found_pmu = false;
+    bool found_fw_cfg = false;
+    bool found_flash = false;
+    bool found_chosen = false;
+    bool found_poweroff = false;
+    bool found_reboot = false;
+    bool found_platform_bus = false;
+    bool found_memory = false;
+    bool found_cpus = false;
+    bool found_soc = false;
+    dtb_foreach_child(root_node, child) {
+        char *childname = dtb_node_name(child);
+        if (strcmp(childname, "pmu") == 0) {
+            found_pmu = true;
+        } else if (strcmp(childname, "fw-cfg@10100000") == 0) {
+            found_fw_cfg = true;
+        } else if (strcmp(childname, "flash@20000000") == 0) {
+            found_flash = true;
+        } else if (strcmp(childname, "chosen") == 0) {
+            found_chosen = true;
+        } else if (strcmp(childname, "poweroff") == 0) {
+            found_poweroff = true;
+        } else if (strcmp(childname, "reboot") == 0) {
+            found_reboot = true;
+        } else if (strcmp(childname, "platform-bus@4000000") == 0) {
+            found_platform_bus = true;
+        } else if (strcmp(childname, "memory@80000000") == 0) {
+            found_memory = true;
+        } else if (strcmp(childname, "cpus") == 0) {
+            found_cpus = true;
+        } else if (strcmp(childname, "soc") == 0) {
+            found_soc = true;
+        }
+    }
+    ok = found_pmu && found_fw_cfg && found_flash && found_chosen && found_poweroff && found_reboot
+        && found_platform_bus && found_memory && found_cpus && found_soc;
+    print_test_result("qemu-virt: dtb_foreach_child '/'", ok);
+
     dtb_node memory_node = dtb_find(devicetree, "/memory");
     ok = memory_node != NULL && strcmp(dtb_node_name(memory_node), "memory@80000000") == 0;
     print_test_result("qemu-virt: dtb_find '/memory'", ok);
